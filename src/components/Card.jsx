633453f45918223a0b23475131e7246393c25a91
Min Thi Ha <minthiha@Mins-MacBook-Air.local>
2023-02-01 14:27:15 +0630

    second

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,12 @@ const Card = ({ el }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <img src={el?.strMealThumb} />
+        <img src={el?.strMealThumb} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title truncate">{el?.strMeal}...</h2>
+        <h2 className="card-title">{el?.strMeal}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
-          <p className="tracking-wider leading-7">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            beatae ipsa veritatis ?
-          </p>
           <Link to={`/${el?.idMeal}`}>
             <button className="btn btn-primary">Details</button>
           </Link>
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -4,22 +4,38 @@ import Card from "./Card";
 
 const Meals = () => {
   const [meals, setMeals] = useState([]);
-  console.log(meals);
-
+  const [categories, setCategories] = useState([]);
+  const [type, setType] = useState("Beef");
   const getMeals = async () => {
     const { data } = await axios.get(
-      "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${type}`
     );
-    // console.log(data?.meals);
     setMeals(data?.meals);
+  };
+
+  const getCategories = async () => {
+    const { data } = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+    );
+    setCategories(data?.meals);
   };
 
   useEffect(() => {
     getMeals();
-  }, []);
+    getCategories();
+  }, [type]);
 
   return (
     <div className="container mx-auto">
+      {categories?.map((cat) => (
+        <button
+          onClick={() => setType(cat?.strCategory)}
+          className="btn btn-sm btn-secondary m-2"
+          key={cat?.strCategory}
+        >
+          {cat?.strCategory}
+        </button>
+      ))}
       <div className="flex flex-wrap justify-center items-center my-10 gap-5">
         {meals?.map((el) => (
           <Card key={el?.idMeal} el={el} />
